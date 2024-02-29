a1c87899c3ee22fdfcdee91acc83ae141ce1f229
abenfredj <120596731+abenfredj@users.noreply.github.com>
2024-02-29 22:38:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello, world this is my first java apps !");
+    System.out.println("Hello, world this is my first java apps 2024 happy year  !");
   }
 }
