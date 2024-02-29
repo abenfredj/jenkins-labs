ced8db3956035aeb4e06640dbc9a6efe6d405318
cloud user <cloud_user_p_2b4142d4@realhandsonlabs.com>
2024-02-29 18:56:42 +0100

    update main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello, world this is my first java app !");
+    System.out.println("Hello, world this is my first java apps !");
   }
 }
